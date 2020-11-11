af7436d830c908f67b009a5cb294981202652e72
Martijn van Schie <martijnvanschie@gmail.com>
2020-11-11 13:57:38 +0100

    Added a nuget puslishing script

---
+++ nuget-push-all.ps1
@@ -0,0 +1,11 @@
+$key = 'oy2psvwh74gn7txeoiragjktrcurygoepol7ml2hp7vaya'
+$source = 'https://api.nuget.org/v3/index.json'
+
+Write-Host "Start packing packages from solution" -ForegroundColor Cyan
+dotnet pack '.\Win32 Interop.sln' -o packages
+
+Get-ChildItem "packages" -Filter *.nupkg | 
+Foreach-Object {
+    Write-Host "Start pushing $($_.FullName)" -ForegroundColor Cyan
+    dotnet nuget push $_.FullName --api-key $key --source $source --skip-duplicate
+}
